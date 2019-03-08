3ff4642897a1cfdb7aedcdce65673344d88a7ee6
Unknown <razvan_roatis@yahoo.com>
2019-03-08 19:02:55 +0200

    Defined ProductData class

--- app/src/main.py
+++ app/src/main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-# from app.src.url import *
+from datetime import datetime
 
 shops = {
     1: 'emag',
@@ -28,8 +28,8 @@ def url_search_generator(shop, category, product_name):   # !!!may need changes!
 
 def get_plain_text(parent):
     if parent is None:
-        return ''
-    return ''.join(x.strip() + ' ' for x in parent.find_all(text=True, recursive=False)).strip()
+        return '-1'
+    return  ''.join(x.strip() + ' ' for x in parent.find_all(text=True, recursive=False)).strip()
 
 
 def find_whole_word(text, word):
@@ -45,11 +45,34 @@ def verify_card_title(title, searched_title):
     return True
 
 
+class RegexCompiles:
+    re_compile_product_id = re.compile('Product-Id=[0-9]*')
+    re_compile_id = re.compile('[0-9]+')
+
+
 def get_product_id(link_to_product):
-    s_matched =  re.compile('Product-Id=[0-9]*').search(link_to_product).group()
+    s_matched =  RegexCompiles.re_compile_product_id.search(link_to_product).group()
     # id_matched = re.compile('[0-9]*').search(s_matched).group()  '*' finds first string as being empty - matches
-    id_matched = re.compile('[0-9]+').search(s_matched).group()
+    id_matched = RegexCompiles.re_compile_id.search(s_matched).group()
     return int(id_matched)
+
+
+class ProductData:
+    def __init__(self, title, old_price, new_price, product_id, link, data_date_time):
+        self.title = title
+        self.old_price = old_price
+        self.new_price = new_price
+        self.product_id = product_id
+        self.link = link
+        self.data_date_time = data_date_time
+
+    def __str__(self) -> str:
+        return 'Title: ' + self.title + '\n' + \
+                'Old price: ' + str(self.old_price) + '\n' + \
+                'New price: ' + str(self.new_price) + '\n' + \
+                'Product id: ' + str(self.product_id) + '\n' + \
+                'Link: ' + self.link + '\n' + \
+                'Date: ' + str(self.data_date_time)
 
 
 def get_data_from_card(html_card, searched_title): # !!!may need changes!!!
@@ -58,16 +81,13 @@ def get_data_from_card(html_card, searched_title): # !!!may need changes!!!
     if not verify_card_title(get_plain_text(title), searched_title):
         return
 
-    old_price = html_card.find('p', class_='product-old-price').find('s')
-    new_price = html_card.find('p', class_='product-new-price')
+    old_price = int(get_plain_text(html_card.find('p', class_='product-old-price').find('s')).replace('.',''))
+    new_price = int(get_plain_text(html_card.find('p', class_='product-new-price')).replace('.',''))
     link_to_product = title['href']
     product_id = get_product_id(link_to_product)
-    print('Title: ' + get_plain_text(title))
-    print('Old price: ' + get_plain_text(old_price))
-    print('New price: ' + get_plain_text(new_price))
-    print('Product id: ' + str(product_id))
-    print('Link: ' + link_to_product)
-    print('--------------------------------')
+    product_data = ProductData(get_plain_text(title), old_price, new_price, product_id, link_to_product, datetime.now())
+    print(product_data)
+    print('--------------------------------------')
 
 
 def run():
@@ -87,5 +107,6 @@ def run():
 
 
 run()
+# print(datetime.now())
 
 #print(get_product_id('https://www.emag.ro/apple-ipad-pro-2018-11-256gb-cellular-silver-mu172hc-a/pd/DJNSR4BBM/?X-Search-Id=ae9e2db81b7196c66864&X-Product-Id=5115157&X-Search-Page=1&X-Search-Position=6&X-Section=search&X-MB=0&X-Search-Action=view'))
